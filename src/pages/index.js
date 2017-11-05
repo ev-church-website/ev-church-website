4f5502c8ef5948d6ec87af3bbf3ad45ddf1dc61c
Andrew Carlson <andrewc0101@gmail.com>
2017-11-05 13:54:47 -0500

    basic start of site

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+export default () =>
+<div>
+  <h1>Hello world!</h1>
+  <p>What a world.</p>
+  <Link to="/page-2">Link</Link>
+</div>
---
+++ src/pages/page-2.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+export default () =>
+<div>
+  <h1>Second Page</h1>
+  <Link to="/">back home</Link>
+</div>
