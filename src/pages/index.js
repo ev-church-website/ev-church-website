888c3a97dce5f31590064dcd7d3307240007e4a7
Andrew Carlson <andrewc0101@gmail.com>
2017-11-05 14:27:57 -0500

    typography

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,8 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 export default () =>
-<div>
-  <h1>Hello world!</h1>
+<div style={{ margin: '3rem auto', maxWidth: 600 }}>
+  <h1>Eagle Valley Church of Christ</h1>
   <p>What a world.</p>
   <Link to="/page-2">Link</Link>
 </div>
