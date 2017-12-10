12cbc2f70d3fe1f030b7f753ca47392afde42c9a
Andrew Carlson <andrewc0101@gmail.com>
2017-12-09 23:47:08 -0500

    minor fixes

--- src/pages/index.js
+++ src/pages/index.js
@@ -94,7 +94,7 @@ export default () =>
         <br />
         <br />
 
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3064.8286554366746!2d-86.3138409487439!3d39.81083510008708!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x886ca910b9db3395%3A0x64efa476da220592!2sEagle+Valley+Church+of+Christ!5e0!3m2!1sen!2sus!4v1512880124863" width="400" height="300" frameBorder="0" style={{border:0}} allowFullScreen></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3064.8286554366746!2d-86.3138409487439!3d39.81083510008708!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x886ca910b9db3395%3A0x64efa476da220592!2sEagle+Valley+Church+of+Christ!5e0!3m2!1sen!2sus!4v1512880124863" width="300" height="300" frameBorder="0" style={{border:0}} allowFullScreen></iframe>
 
       </Tile>
 
