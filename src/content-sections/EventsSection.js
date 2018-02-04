cf3bb860de7493163900e81d57e6f353372b3d1d
Andrew Carlson <andrewc0101@gmail.com>
2018-02-04 16:24:20 -0500

    improvements to events

--- src/content-sections/EventsSection.js
+++ src/content-sections/EventsSection.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import SectionHeading from '../shared/SectionHeading.js';
 import * as BaseClasses from "../styles/BaseClasses";
+import dateFormat from 'dateformat';
+import marked from 'marked';
 
 export default ({ data }) => {
   return (
-    <div className={`${BaseClasses.container}`}>
+    <div className={`${BaseClasses.container}`} css={{
+      paddingBottom: '2rem'
+    }}>
 
       <SectionHeading>UPCOMING EVENTS</SectionHeading>
 
@@ -12,22 +16,20 @@ export default ({ data }) => {
 
         <thead>
           <tr>
-            <th>Date</th>
-            <th>Name</th>
+            <th style={{width: '90px'}}>Date</th>
+            <th style={{width: '210px'}}>Name</th>
             <th>Description</th>
           </tr>
         </thead>
 
         {data.map((event) => {
 
-          console.dir(event);
-
           return (
 
             <tr>
-              <td>{event.date}</td>
-              <td>{event.title}</td>
-              <td>{event.description}</td>
+              <td css={{verticalAlign: 'top'}}>{formatDate(event.date)}</td>
+              <td css={{verticalAlign: 'top'}}>{event.title}</td>
+              <td dangerouslySetInnerHTML={{ __html: marked(event.description) }} ></td>
             </tr>
 
           );
@@ -38,4 +40,20 @@ export default ({ data }) => {
 
     </div>
   )
+}
+
+function formatDate(dateString) {
+
+  var dateToFormat = new Date(dateString);
+
+  if (dateToFormat.getHours() === 0) {
+
+    return dateFormat(dateToFormat, "mmm d");
+
+  } else {
+
+    return dateFormat(dateToFormat, "mmm d h:MM TT");
+
+  }
+
 }
