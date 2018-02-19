2b3b45ed111470470c34af4357d9f02e37f7103a
Andrew Carlson <andrewc0101@gmail.com>
2018-02-18 23:17:21 -0500

    improvements to contact us section

--- src/content-sections/ContactUsSection.js
+++ src/content-sections/ContactUsSection.js
@@ -2,6 +2,7 @@ import React from 'react';
 import SectionHeading from '../shared/SectionHeading.js';
 import * as BaseClasses from '../styles/BaseClasses';
 import FormElement from '../shared/FormElement.js';
+import update from 'immutability-helper';
 
 const elementWidth = '400px';
 
@@ -16,23 +17,50 @@ export default class ContactUsSection extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {};
+
+    this.state = {
+      isSubmitted: false,
+      formData: this.getInitialFormData(),
+    };
   }
 
-  handleChange = (e) => {
-    this.setState({[e.target.name]: e.target.value});
+  getInitialFormData = () => {
+
+    return {
+      name: '',
+      email: '',
+      phone: '',
+      message: '',
+    };
+
   };
 
-  handleSubmit = e => {
+  handleChange = (e) => {
+
+    this.setState({formData: update(this.state.formData, {[e.target.name]: {$set: e.target.value}})});
+
+  };
+
+  handleSubmit = (e) => {
+
     fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "contact", ...this.state })
+      body: encode({ "form-name": "contact", ...this.state.formData })
     })
-      .then(() => alert("Success!"))
+      .then(this.handleSuccess)
       .catch(error => alert(error));
 
     e.preventDefault();
+  };
+
+  handleSuccess = () => {
+
+    this.setState({
+      isSubmitted: true,
+      formData: this.getInitialFormData(),
+    });
+
   };
 
   render() {
@@ -52,6 +80,7 @@ export default class ContactUsSection extends React.Component {
             display: 'flex',
             flexDirection: 'column',
             alignItems: 'center',
+            marginBottom: 0,
           }}
           onSubmit={this.handleSubmit}
         >
@@ -62,29 +91,38 @@ export default class ContactUsSection extends React.Component {
 
           <FormElement label="Name" width={elementWidth}>
 
-            <input type="text" name="name" css={{width: '100%'}} onChange={this.handleChange}/>
+            <input type="text" name="name" css={{width: '100%'}} value={this.state.formData.name} onChange={this.handleChange}/>
 
           </FormElement>
 
           <FormElement label="Email" width={elementWidth}>
 
-            <input type="text" name="email" css={{width: '100%'}} onChange={this.handleChange}/>
+            <input type="text" name="email" css={{width: '100%'}} value={this.state.formData.email}  onChange={this.handleChange}/>
 
           </FormElement>
 
           <FormElement label="Phone" width={elementWidth}>
 
-            <input type="text" name="phone" css={{width: '100%'}} onChange={this.handleChange}/>
+            <input type="text" name="phone" css={{width: '100%'}} value={this.state.formData.phone} onChange={this.handleChange}/>
 
           </FormElement>
 
           <FormElement label="Message" width={elementWidth}>
 
-            <textarea name="message" css={{width: '100%', height: '10rem'}} onChange={this.handleChange}/>
+            <textarea name="message" css={{width: '100%', height: '10rem'}} value={this.state.formData.message} onChange={this.handleChange}/>
 
           </FormElement>
 
           <button type="submit">Send</button>
+
+          <div css={{
+            paddingTop: '1rem',
+            visibility: (this.state.responseMessage ? 'visible' : 'hidden')
+          }}>
+
+            Thanks! We have received your message.
+
+          </div>
 
         </form>
 
