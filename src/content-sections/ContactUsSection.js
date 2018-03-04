04224d93e4f0ebfdacea6dd12e9fb8618b08fd0b
Andrew Carlson <andrewc0101@gmail.com>
2018-03-04 16:06:14 -0500

    using formspree for forms instead

--- src/content-sections/ContactUsSection.js
+++ src/content-sections/ContactUsSection.js
@@ -15,54 +15,6 @@ function encode(data) {
 
 export default class ContactUsSection extends React.Component {
 
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      isSubmitted: false,
-      formData: this.getInitialFormData(),
-    };
-  }
-
-  getInitialFormData = () => {
-
-    return {
-      name: '',
-      email: '',
-      phone: '',
-      message: '',
-    };
-
-  };
-
-  handleChange = (e) => {
-
-    this.setState({formData: update(this.state.formData, {[e.target.name]: {$set: e.target.value}})});
-
-  };
-
-  handleSubmit = (e) => {
-
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "contact", ...this.state.formData })
-    })
-      .then(this.handleSuccess)
-      .catch(error => alert(error));
-
-    e.preventDefault();
-  };
-
-  handleSuccess = () => {
-
-    this.setState({
-      isSubmitted: true,
-      formData: this.getInitialFormData(),
-    });
-
-  };
-
   render() {
     return (
       <div className={`${BaseClasses.container}`} css={{
@@ -72,59 +24,41 @@ export default class ContactUsSection extends React.Component {
         <SectionHeading>CONTACT US</SectionHeading>
 
         <form
-          name="contact"
+          action="https://formspree.io/evchurchwebsite@gmail.com"
           method="post"
-          data-netlify="true"
-          data-netlify-honeypot="bot-field"
           css={{
             display: 'flex',
             flexDirection: 'column',
             alignItems: 'center',
             marginBottom: 0,
           }}
-          onSubmit={this.handleSubmit}
         >
-
-          <p hidden>
-            <input name="bot-field" onChange={this.handleChange}/>
-          </p>
 
           <FormElement label="Name" width={elementWidth}>
 
-            <input type="text" name="name" css={{width: '100%'}} value={this.state.formData.name} onChange={this.handleChange}/>
+            <input type="text" name="name" css={{width: '100%'}} />
 
           </FormElement>
 
           <FormElement label="Email (optional)" width={elementWidth}>
 
-            <input type="text" name="email" css={{width: '100%'}} value={this.state.formData.email}  onChange={this.handleChange}/>
+            <input type="text" name="email" css={{width: '100%'}} />
 
           </FormElement>
 
           <FormElement label="Phone (optional)" width={elementWidth}>
 
-            <input type="text" name="phone" css={{width: '100%'}} value={this.state.formData.phone} onChange={this.handleChange}/>
+            <input type="text" name="phone" css={{width: '100%'}} />
 
           </FormElement>
 
           <FormElement label="Message" width={elementWidth}>
 
-            <textarea name="message" css={{width: '100%', height: '10rem'}} value={this.state.formData.message} onChange={this.handleChange}/>
+            <textarea name="message" css={{width: '100%', height: '10rem'}} />
 
           </FormElement>
 
-          <div data-netlify-recaptcha></div>
-
           <button type="submit">Send</button>
-
-          <div css={{
-            paddingTop: '1rem',
-            visibility: (this.state.isSubmitted ? 'visible' : 'hidden')
-          }}>
-
-            Thanks! We have received your message.
-
-          </div>
 
         </form>
 
