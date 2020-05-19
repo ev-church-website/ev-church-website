07801f5387717c6bc4126f249dfa2ca33d69381f
jerasor <jeremyrasor97@gmail.com>
2020-05-19 15:13:08 -0400

    Integrate Google Calendar API for events

--- src/content-sections/EventsSection.js
+++ src/content-sections/EventsSection.js
@@ -4,8 +4,9 @@ import * as BaseClasses from "../styles/BaseClasses";
 import dateFormat from 'dateformat';
 import marked from 'marked';
 import Media from '../styles/Media';
+import EventsList from './EventsList.js';
 
-export default ({ data }) => {
+export default () => {
   return (
     <div className={`${BaseClasses.container}`} css={{
       padding: '0rem 1rem 2rem'
@@ -13,10 +14,10 @@ export default ({ data }) => {
 
       <SectionHeading>UPCOMING EVENTS</SectionHeading>
 
-      <div css={{display: 'flex', justifyContent:'center', paddingBottom: '1rem'}}>
+      <div css={{ display: 'flex', justifyContent: 'center', paddingBottom: '1rem' }}>
         <a
           className={`${BaseClasses.link}`}
-          css={{margin: '0 1rem 0', textAlign: 'center'}}
+          css={{ margin: '0 1rem 0', textAlign: 'center' }}
           href="https://calendar.google.com/calendar/embed?src=shkmbft44mds4g3ngvjfl3cffk%40group.calendar.google.com&ctz=America%2FNew_York"
           target="_blank"
         >
@@ -24,7 +25,7 @@ export default ({ data }) => {
         </a>
         <a
           className={`${BaseClasses.link}`}
-          css={{margin: '0 1rem 0', textAlign: 'center'}}
+          css={{ margin: '0 1rem 0', textAlign: 'center' }}
           href="https://calendar.google.com/calendar/b/0/r?cid=c2hrbWJmdDQ0bWRzNGczbmd2amZsM2NmZmtAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ"
           target="_blank"
         >
@@ -32,97 +33,9 @@ export default ({ data }) => {
         </a>
       </div>
 
-      <div css={{
-        display: 'none',
-        [Media.phone]: {
-          display: 'inline',
-        }
-      }}>
-        {data.map((event, eventIndex) => {
 
-          return (
-
-            <div key={eventIndex} css={{
-              paddingBottom: '1rem',
-            }}>
-
-              <div css={{
-                fontWeight: 'bold',
-                fontSize: '1.25rem',
-                textAlign: 'center',
-                paddingBottom: '.5rem'
-              }}>
-                {event.title}
-              </div>
-
-              <div css={{
-                textAlign: 'center',
-                fontStyle: 'italic',
-                paddingBottom: '.5rem',
-              }}>
-                {formatDate(event.date)}
-              </div>
-
-              <div dangerouslySetInnerHTML={{ __html: marked(event.description) }} ></div>
-
-            </div>
-
-          );
-
-        })}
-
-      </div>
-
-
-      <table css={{
-        display: 'inline',
-        [Media.phone] : {
-          display: 'none'
-        }
-      }}>
-
-        <thead>
-          <tr>
-            <th style={{width: '150px'}}>Date</th>
-            <th style={{width: '250px'}}>Name</th>
-            <th style={{width: '500px'}}>Description</th>
-          </tr>
-        </thead>
-
-        <tbody>
-        {data.map((event, eventIndex) => {
-
-          return (
-
-            <tr key={eventIndex}>
-              <td css={{verticalAlign: 'top'}}>{formatDate(event.date)}</td>
-              <td css={{verticalAlign: 'top'}}>{event.title}</td>
-              <td dangerouslySetInnerHTML={{ __html: marked(event.description) }} ></td>
-            </tr>
-
-          );
-
-        })}
-        </tbody>
-
-      </table>
+      <EventsList />
 
     </div>
   )
-}
-
-function formatDate(dateString) {
-
-  var dateToFormat = new Date(dateString);
-
-  if (dateToFormat.getHours() === 0) {
-
-    return dateFormat(dateToFormat, "mmm d");
-
-  } else {
-
-    return dateFormat(dateToFormat, "mmm d h:MM TT");
-
-  }
-
 }
